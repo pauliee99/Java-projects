765aeec5dd50c0d6e7026c905ea42cc2618650c0
Pavlos Andreou <pavlosan67@gmail.com>
2019-12-22 15:06:22 +0200

    javis start

--- javis/src/javisApp.java
+++ javis/src/javisApp.java
@@ -1,6 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
 
+/**
+ *
+ * @author pavlos
+ */
 public class javisApp {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        Scanner vechicle = new Scanner(System.in);
+        System.out.println("give type: (car or bike)");
+        String type = vechicle.nextLine();
+    }
+
 }
