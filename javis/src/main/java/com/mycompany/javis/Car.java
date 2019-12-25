ea5353661b226f221b8524620c1dd9312b8d6bf5
Pavlos Andreou <pavlosan67@gmail.com>
2019-12-25 23:29:01 +0200

    javis

---
+++ javis/src/main/java/com/mycompany/javis/Car.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package com.mycompany.javis;
+
+/**
+ *
+ * @author pavlos
+ */
+public class Car {
+    private String tipos;
+    private String kafsimo;
+    private int ippodinami;
+    private int troxoi;
+    private int kivismos;
+    private int kostos;
+    
+    public Car(String type, String gas, int hp, int wheels, int blocks, int cost ){
+        tipos = type;
+        kafsimo = gas;
+        ippodinami = hp;
+        troxoi = wheels;
+        kivismos = blocks;
+        kostos = cost;
+    }
+    
+    public void kratisi(String type){
+         if (type == "car"){
+             //afto tha efanizi tin lista me ta diathesima aftikinita ke pote to theli
+         }
+    }
+        
+
+}
---
+++ javis/src/main/java/com/mycompany/javis/javisApp.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package com.mycompany.javis;
+
+import java.util.*; 
+import java.util.stream.*; 
+import java.util.Scanner;
+
+/**
+ *
+ * @author pavlos
+ */
+public class javisApp {
+    public static void main(String[] args) {
+    String availableCars[] = {"car1", "car2", "car3"};
+    String availableBikes[] = {"bike1", "bike2"};
+    Scanner scan = new Scanner(System.in);
+    System.out.println("give type: (car or bike)");
+    String type = scan.nextLine(); //reads type of vechicle
+        
+    if (type.equals("car")) {
+      	Stream.of(availableCars).forEach(System.out::println);
+       	switch( type ) {
+            case "car1":
+        			
+            }
+        }
+        else if (type.equals("bike")) {
+           Stream.of(availableBikes).forEach(System.out::println);
+        }
+        else {
+            System.out.println("wrong input");
+        }
+        
+        
+    }
+    
+  
+}
